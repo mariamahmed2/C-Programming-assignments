c2d27a05b95ec1d9e314c508621147f8b9f4229d
Mariam A.Fathy <64314238+mariamahmed2@users.noreply.github.com>
2021-04-12 19:01:21 +0200

    Add files via upload
    C program to take from the user 5 digits integer and reverse its digits and add them.
    Code without using loops

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/* Program to take from the user 5 digits and reverse its digits*/
+int main()
+{
+    int number;
+    int rev;
+    int next_digit;
+    int last_digit;
+    int sum = 0;
+
+    printf ("the input integer is: ");
+    scanf("%d", &number);
+
+    last_digit = number - ((number / 10) * 10);
+    rev = last_digit;
+    sum = sum + last_digit;
+
+    next_digit = (number / 10) - ((number / 100) * 10);
+    rev = (rev * 10) + next_digit;
+    sum = sum + next_digit;
+
+    next_digit = (number / 100) - ((number / 1000) * 10);
+    rev = (rev * 10) + next_digit;
+    sum = sum + next_digit;
+
+    next_digit = (number / 1000) - ((number / 10000) * 10);
+    rev = (rev * 10) + next_digit;
+    sum = sum + next_digit;
+
+    next_digit = (number / 10000) - ((number / 100000) * 10);
+    rev = (rev * 10) + next_digit;
+    sum = sum + next_digit;
+
+    printf ("The reversed integer is: %d",rev);
+    printf("\n The Sum of the digits = %d", sum);
+
+    return 0;
+}
