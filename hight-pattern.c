3cbf810009387f1bc84048eb16af605626eebfe6
Mariam A.Fathy <64314238+mariamahmed2@users.noreply.github.com>
2021-05-01 17:58:15 +0200

    Add files via upload

---
+++ hight-pattern.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+        printf("Hight: ");
+        scanf("%d", &n);
+
+    
+
+    for (int i = 0; i < n; i++)
+    {
+       for (int j = 0; j < n; j++)
+       {
+           if (j > i)
+           {
+               printf(" ");
+           }   
+
+            else
+            {
+               printf("*");
+            }  
+
+       }
+       printf("\n");
+    }
+return 0;
+}
