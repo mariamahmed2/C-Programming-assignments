381a933f97d5da8e18eeaf1fe2d3fa11f9a60fc5
Mariam A.Fathy <64314238+mariamahmed2@users.noreply.github.com>
2021-05-06 00:24:45 +0200

    Add files via upload

---
+++ triangle-01.c
@@ -0,0 +1,31 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main()
+{
+
+    int i, j, n;
+
+    printf("Enter n rows: ");
+    scanf("%d", &n);
+
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+        
+            if(j % 2 == 0)
+            {
+                printf("0");
+            }
+            else
+            {
+                printf("1");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
